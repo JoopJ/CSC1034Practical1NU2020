e646e0f05cf6983725f5476222f70a427594bbf9
Josh Oppenheimer <c0039786@newcastle.ac.uk>
2020-10-21 17:16:25 +0100

    Add Models and Renderer to hello_world and Update requirements

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,4 @@
+from direct.showbase.ShowBase import ShowBase
 import sys
 import platform
 
@@ -6,3 +7,20 @@ print("Hello")
 print( 1, sys.version)
 print( 2, platform.python_implementation())
 print( 3, sys.executable)
+
+
+class MyApp(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+        # Apply scale and position transform on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+app = MyApp()
+app.run()
