372d0ab072c659094e545a4f431fbb0e3c834eb9
Josh Oppenheimer <c0039786@newcastle.ac.uk>
2020-10-28 15:23:26 +0000

    Add 4 arg parse options
    scale=1 increases the scale of the panda, cam-speed sets the speed at which the camera rotates around the panda, cam-spin-direction changes the direction the camera spins from right to left, flip-camera flips the camera upside down. New function setCameraPosition used to set the camera position without spinning it around the panda.

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -6,6 +6,10 @@ import argparse
 def cli():
     parser = argparse.ArgumentParser(prog = "walking_panda")
     parser.add_argument("--no-rotate", help="Suppress Rotation", action="store_true")
+    parser.add_argument("--scale=1", help="Increase Scale", action="store_const", dest="set_scale", const=0.01, default=0.005)
+    parser.add_argument("--cam-speed", help="Set Speed of Camera Rotation", action="store", default=6)
+    parser.add_argument("--cam-spin-direction", help="Change Camera Spin Direction to Spin Left", action="store_const", const=-1,default=1)
+    parser.add_argument("--flip-camera", help="Flip the Camera Upside Down", action="store_const", const=180, default=0)
     args = parser.parse_args()
 
     walking = WalkingPanda(**vars(args))
--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -5,7 +5,7 @@ from direct.actor.Actor import Actor
 
 class WalkingPanda(ShowBase):
 
-    def __init__(self, no_rotate=False):
+    def __init__(self, no_rotate=False,set_scale=0.005,cam_speed=6,cam_spin_direction=1, flip_camera=0):
         ShowBase.__init__(self)
         # Load the environment model.
         self.scene = self.loader.loadModel("models/environment")
@@ -16,29 +16,29 @@ class WalkingPanda(ShowBase):
         self.scene.setPos(-8, 42, 0)
 
         # Check wether to rotate the camera or to keep it still
-        if (not no_rotate):
-            # Add the spinCameraTask procedure to the task manager.
-            self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-        else:
+        if (no_rotate or flip_camera != 0):
             # Add the setCameraPosition procedure to the task manager.
-            self.taskMgr.add(self.setCameraPosition, "SetCameraPositionTask")
+            self.taskMgr.add(self.setCameraPosition, "SetCameraPositionTask", extraArgs=[flip_camera], appendTask=True)
+        else:
+            # Add the spinCameraTask procedure to the task manager.
+            self.taskMgr.add(self.spinCameraTask, "SpinCameraTask", extraArgs=[cam_speed,cam_spin_direction], appendTask=True)
 
         # Load and transform the panda actor.
         self.pandaActor = Actor("models/panda-model",{"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.setScale(set_scale, set_scale, set_scale)
         self.pandaActor.reparentTo(self.render)
         # Loop its animation.
         self.pandaActor.loop("walk")
 
     # Define a procedure to move the camera.
-    def spinCameraTask(self, task):
-        angleDegrees = task.time * 6.0
+    def spinCameraTask(self, cam_speed, cam_spin_direction, task):
+        angleDegrees = task.time * cam_speed * cam_spin_direction
         angleRadians = angleDegrees * (pi / 180.0)
         self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees, 0, 0)
         return Task.cont
     # Define a procedure to position the camera infront of the panda
-    def setCameraPosition(self, task):
+    def setCameraPosition(self, flip_camera, task):
         self.camera.setPos(0,-20,3)
-        self.camera.setHpr(0,0,0)
-        return Task.done
+        self.camera.setHpr(0,0,flip_camera)
+        return Task.cont
